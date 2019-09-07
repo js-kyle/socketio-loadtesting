67c558e30c92102ca4ee3fe68c1a2e3c203d3dea
Kyle Martin <kyle.martin@hotmail.co.nz>
2019-09-07 23:09:31 +1200

    add advanced test scenario

--- load-test/custom.js
+++ load-test/custom.js
@@ -2,6 +2,7 @@ const faker = require('faker');
 
 module.exports.getChatData = (userContext, events, done) => {
   userContext.vars.name = faker.name.findName();
-  userContext.vars.message = `Hello from ${faker.address.city()}`;
+  userContext.vars.greeting = `Hello! I'm from ${faker.address.city()}`;
+  userContext.vars.goodbye = `Goodbye, I will be back on ${faker.date.weekday()}`;
   done();
 };
