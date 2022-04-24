7f030ae0d1310f2c5dcb27bb5f2ec0e0608bd3e8
Thomas BOKO <bokothomas@yahoo.fr>
2022-04-24 19:12:14 +0100

    update faker syntax

--- load-test/custom.js
+++ load-test/custom.js
@@ -1,4 +1,4 @@
-const faker = require('faker');
+const {faker} = require('@faker-js/faker');
 
 module.exports.getChatData = (userContext, events, done) => {
   userContext.vars.name = faker.name.findName();
